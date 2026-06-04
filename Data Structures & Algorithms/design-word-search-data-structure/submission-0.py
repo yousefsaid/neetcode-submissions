fc5c26664c6fedee25b37554506db0fa4eef494b
Yousef Said <53379110+yousefsaid@users.noreply.github.com>
2026-06-03 22:36:15 -0400

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.child = {}
+        self.word = False
+class WordDictionary:
+
+    def __init__(self):
+        self.node = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        curr = self.node
+
+        for c in word:
+            if c not in curr.child:
+                curr.child[c] = TrieNode()
+            curr = curr.child[c]
+        curr.word = True
+        
+
+    def search(self, word: str) -> bool:
+        def dfs(j, node):
+            curr = node
+
+            for i in range(j, len(word)):
+                c = word[i]
+
+                if c == ".":
+                    for child in curr.child.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                
+                else:
+                    if c not in curr.child:
+                        return False
+                    curr = curr.child[c]
+            return curr.word
+        
+        return dfs(0, self.node)
+        
+        
