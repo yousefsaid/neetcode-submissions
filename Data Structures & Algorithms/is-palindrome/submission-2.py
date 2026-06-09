612ee234a0942910953296beaed6b18f72e044e4
Yousef Said <53379110+yousefsaid@users.noreply.github.com>
2026-06-08 21:24:18 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        sanitizedS =  ("".join(s.split())).lower()
+        L = 0
+        R = len(sanitizedS) - 1
+
+        while L < R:
+            
+
+            while L < R and not sanitizedS[L].isalnum():
+                L += 1
+            
+            while L < R and not sanitizedS[R].isalnum():
+                R -= 1
+            
+            if sanitizedS[L] == sanitizedS[R]:
+                L += 1
+                R -= 1
+            else:
+                return False
+        return True
+
+
+            
